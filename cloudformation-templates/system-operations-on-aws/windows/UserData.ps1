8c6790a5f17799df91969e0d50f080f87ccba56c
Bruno Lopes <37803210+brunokktro@users.noreply.github.com>
2020-08-15 12:23:10 -0300

    Add files via upload

--- cloudformation-templates/system-operations-on-aws/windows/UserData.ps1
+++ cloudformation-templates/system-operations-on-aws/windows/UserData.ps1
@@ -4,4 +4,9 @@ New-Item -ItemType directory -Path 'C:\temp', 'C:\temp\aws'
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile('https://s3.amazonaws.com/aws-cli/AWSCLI64.msi', 'C:\temp\aws\AWSCLI64.msi')
 Start-Process 'C:\temp\aws\AWSCLI64.msi' -ArgumentList /qn -Wait
+
+# Install IIS and Web Management Tools.
+Import-Module ServerManager
+install-windowsfeature web-server, web-webserver -IncludeAllSubFeature
+install-windowsfeature web-mgmt-tools
 </powershell>
